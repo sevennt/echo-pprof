d33b7724e78a5e42671aa3c7ece449b8d332b0b0
robotomize <robotomize@sakh.com>
2019-12-10 10:50:54 +1100

    Bug fixes

--- example/example.go
+++ example/example.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/labstack/echo"
-	"github.com/sevenNt/echo-pprof"
+	echopprof "echo-pprof"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
--- pprof_test.go
+++ pprof_test.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 )
 
 func newServer() *echo.Echo {
